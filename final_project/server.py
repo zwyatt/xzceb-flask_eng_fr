5e8059420c637046f30e42b222a5ce9447e7c497
zwyatt <zwyatt.work@gmail.com>
2023-04-14 01:21:22 -0400

    Fixed render_template not returning

--- final_project/server.py
+++ final_project/server.py
@@ -30,7 +30,7 @@ def frenchToEnglish():
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-    render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
